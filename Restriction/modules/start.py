287f5856b4e59cd1fa811049ac0ae79fa88ff36d
ramdin23 <134823485+ramdin23@users.noreply.github.com>
2024-06-26 19:45:03 +0530

    Update start.py

--- Restriction/modules/start.py
+++ Restriction/modules/start.py
@@ -187,6 +187,64 @@ async def plan_command(_,message):
         caption=PRE_TEXT,
         buttons=Button
 	 )
+---------------
+
+
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from pyrogram import Client , filters
+
+@Client.on_callback_query(filters.regex('upgrade'))
+async def upgrade(bot,update):
+	text = """**Free Plan User**
+	Daily  Upload limit 2GB
+	Price 0
+	
+	**VIP 1 ** 
+	Daily  Upload  limit 10GB
+	Price Rs 55  🇮🇳/🌎 0.67$  per Month
+	
+	**VIP 2 **
+	Daily Upload limit 50GB
+	Price Rs 80  🇮🇳/🌎 0.97$  per Month
+	
+	Pay Using Upi I'd ```divindass@fbl```
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/webx_admin_bot")], 
+        			[InlineKeyboardButton("PayPal 🌎",url = "https://t.me/webx_admin_bot"),
+        			InlineKeyboardButton("UPI ",url = "https://t.me/webx_admin_bot")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
+	
+
+@Client.on_message(filters.private & filters.command(["upgrade"]))
+async def upgradecm(bot,message):
+	text = """**Free Plan User**
+	Daily  Upload limit 2GB
+	Price 0
+	
+	**VIP 1 ** 
+	Daily  Upload  limit 10GB
+	Price Rs 55  🇮🇳/🌎 0.67$  per Month
+	
+	**VIP 2 **
+	Daily Upload limit 50GB
+	Price Rs 80  🇮🇳/🌎 0.97$  per Month
+	
+	
+	Pay Using Upi I'd ```divindass@fbl```
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/webx_admin_bot")], 
+        			[InlineKeyboardButton("PayPal 🌎",url = "https://t.me/webx_admin_bot"),
+        			InlineKeyboardButton("Paytm",url = "https://t.me/webx_admin_bot")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
+
+
+----------------
 
 @app.on_message(filters.command("start"))
 async def start(_,message):
